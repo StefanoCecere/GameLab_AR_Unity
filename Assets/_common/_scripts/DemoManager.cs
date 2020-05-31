22739b824820bdc40b0524e959684b8d0eb3dbad
Stefano Cecere <stefano.cecere@krur.com>
2020-05-31 17:17:50 +0200

    _common

---
+++ Assets/_common/_scripts/DemoManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KRUR.ARdemo
+{
+    public class DemoManager : MonoBehaviour
+    {
+        private static DemoManager _instance;
+
+        public static DemoManager I
+        {
+            get { return _instance; }
+        }
+
+        void Awake()
+        {
+            if (_instance != null && _instance != this) {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            _instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        public void ChangeScene(int sceneNumber)
+        {
+            //sceneNumber -= 1;
+            SceneManager.LoadScene(sceneNumber, LoadSceneMode.Single);
+            //Debug.Log("carico " + sceneNumber);
+        }
+
+    }
+}
---
+++ Assets/_common/_scripts/DemoScenesMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+
+namespace KRUR.ARdemo
+{
+    public class DemoScenesMenu : MonoBehaviour
+    {
+        public GameObject MenuItemPrefab;
+
+        void Start()
+        {
+            clearAllChild();
+            buildMenu();
+        }
+
+        public void ChangeScene(int sceneNumber)
+        {
+            DemoManager.I.ChangeScene(sceneNumber);
+        }
+
+        private void buildMenu()
+        {
+            GameObject item = null;
+            var totScenes = SceneManager.sceneCountInBuildSettings;
+            for (int i = 0; i < totScenes; i++) {
+                int sceneIndex = new int();
+                sceneIndex = i;
+                //Debug.Log("scena: " + SceneUtility.GetScenePathByBuildIndex(i));
+                item = Instantiate(MenuItemPrefab, transform);
+                item.transform.Find("Label").GetComponent<TextMeshProUGUI>().text = getSceneName(SceneUtility.GetScenePathByBuildIndex(i));
+                item.GetComponent<Button>().onClick.AddListener(delegate () { ChangeScene(sceneIndex); });
+            }
+        }
+
+        private string getSceneName(string ScenePath)
+        {
+            var namearray = Path.GetFileName(ScenePath).Split('.');
+            return namearray[0];
+        }
+
+        private void clearAllChild()
+        {
+            foreach (Transform child in transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+    }
+}
